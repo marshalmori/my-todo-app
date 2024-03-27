06dfe17be64f0eb83ba0b9cf91b7ae8dda62935f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-03-27 17:07:24 -0300

    Create delete todo

--- web.py
+++ web.py
@@ -5,8 +5,8 @@ todos = functions.get_todos()
 
 
 def add_todo():
-    todo_input = st.session_state['new_todo']
-    todos.append(todo_input + '\n')
+    todo_input = st.session_state['new_todo'] + '\n'
+    todos.append(todo_input)
     functions.write_todos(todos)
 
 
@@ -15,7 +15,14 @@ st.subheader('This is my todo app.')
 st.write('This app is to increase your productivity.')
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label='Enter to apply', placeholder='Add new todo...', on_change=add_todo, key='new_todo')
+
+# st.session_state
